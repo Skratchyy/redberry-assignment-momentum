858916054733f5744dc731ba774922e8b8e67764
Skratchyy <gabichvadzezura@gmail.com>
2025-03-21 20:36:24 +0400

    More router issues

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import Assignment from "./views/Assignment";
 
 const routes = createBrowserRouter([
   {
-    path: "/",
+    path: "/redberry-assignment-momentum/",
     element: <Header />,
     children: [
       {
